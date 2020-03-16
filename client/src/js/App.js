04a1a00bfbe88ed1bf1048a0cdeae13d0ab7e4ef
Hugo Guerrier <hugo91@netcourrier.com>
2020-03-16 16:30:33 +0100

    Create the Anny logo + Format the client welcome page

---
+++ client/src/js/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../img/logo.png';
+import '../css/App.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            test: 0
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to Anny (Not developed yet...)</h2>
+                </div>
+                <p className="App-intro">
+                    You'll have to wait fot the developer to work on it...
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
